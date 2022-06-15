0b1a4c2658b83bffbce5c99bcdedcfec406055c4
Italo Ferreira <grd788@gmail.com>
2022-06-14 23:43:13 -0500

    feat :sparkles: alters package and routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,24 @@
 import { Footer } from './components/Footer'
 import { Header } from './components/Header'
 import { Home } from './components/Home'
-import { Introduction } from './components/Home/Introduction'
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import { GlobalStyles } from './styles/globalStyles'
+import { SignUp } from './pages/SignIn'
+import { SignIn } from './pages/SignUp'
 
 export function App() {
   return (
     <>
-      <Header />
-      <Home />
-      <GlobalStyles />
-      <Footer />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/signin" element={<SignIn />} />
+        </Routes>
+        <GlobalStyles />
+        <Footer />
+      </BrowserRouter>
     </>
   )
 }
