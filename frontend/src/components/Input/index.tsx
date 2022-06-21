71e5e1f78862ccaafa6ac24f96ae1a08c0320746
Italo Ferreira <grd788@gmail.com>
2022-06-20 23:34:04 -0500

    feat :sparkles: add estyle to Input component

---
+++ frontend/src/components/Input/index.tsx
@@ -0,0 +1,16 @@
+import { InputWrapper } from "./style"
+
+type InputType = {
+  name: string
+  type: string
+  placeholder?: string
+  value?: string
+}
+
+export function Input({ name, type, value, placeholder }: InputType) {
+  return (
+    <InputWrapper>
+      <input className="input" type={type} id={name} name={name} value={value} placeholder={placeholder} />
+    </InputWrapper>
+  )
+}
---
+++ frontend/src/components/Input/style.ts
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+
+
+export const InputWrapper = styled.div`
+
+
+  .input {
+    width: 278px;
+    height: 45px;
+    background-color: #121212;
+    margin-bottom: 15px;
+
+
+    ::placeholder {
+      font-family: var(--type-frist);
+      font-size: 0.875rem;
+      font-weight: medium;
+      color: #767474;
+      padding: 12px 0px 12px 5px;
+    }
+  }
+`
