5ded81bdcce3ef187e5a3bdd92333c22ccde852c
Italo Ferreira <grd788@gmail.com>
2022-06-23 23:39:46 -0500

    feat :sparkles: sign in page and style

--- frontend/src/pages/signin/index.tsx
+++ frontend/src/pages/signin/index.tsx
@@ -1,8 +1,28 @@
+import { Button } from '../../components/Button'
+import { Input } from '../../components/Input'
+import { FormWrapper, SignInText, SignInWrapper, SignUpText } from './style'
+import { Link } from 'react-router-dom'
 
 export function SignIn() {
   return (
     <>
-      <div></div>
+      <SignInWrapper>
+        <SignInText>Faça seu login em nossa plataforma</SignInText>
+        <FormWrapper>
+          <Input name="email" type="email" placeholder="Seu E-mail" value="" />
+          <Input
+            name="password"
+            type="password"
+            placeholder="Sua Senha"
+            value=""
+          />
+          <Button text="Entrar" margin="15px 0" />
+          <SignUpText>
+            Não tem conta?
+            <Link to="/signup">Registre-se</Link>
+          </SignUpText>
+        </FormWrapper>
+      </SignInWrapper>
     </>
   )
 }
--- frontend/src/pages/signin/style.ts
+++ frontend/src/pages/signin/style.ts
@@ -1,4 +1,17 @@
 import styled from "styled-components";
+import { Container } from "../../styles/layout/Container";
 
+export const SignInWrapper = styled(Container)`
+`
 
+export const SignInText = styled.p`
+
+`
+
+export const FormWrapper = styled.form`
+
+`
+
+export const SignUpText = styled.p`
+`
 
