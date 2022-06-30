c28bebde2539a952121553bc8329af7c9262f428
Italo Ferreira <grd788@gmail.com>
2022-06-29 23:25:06 -0500

    in progess :construction: page sign in construction

--- frontend/src/pages/signin/index.tsx
+++ frontend/src/pages/signin/index.tsx
@@ -2,6 +2,8 @@ import { Button } from '../../components/Button'
 import { Input } from '../../components/Input'
 import { FormWrapper, SignInText, SignInWrapper, SignUpText } from './style'
 import { Link } from 'react-router-dom'
+import { Envelope, LockSimple } from 'phosphor-react'
+import { InputDiv, InputItem } from '../signup/style'
 
 export function SignIn() {
   return (
@@ -9,13 +11,28 @@ export function SignIn() {
       <SignInWrapper>
         <SignInText>Faça seu login em nossa plataforma</SignInText>
         <FormWrapper>
-          <Input name="email" type="email" placeholder="Seu E-mail" value="" />
-          <Input
-            name="password"
-            type="password"
-            placeholder="Sua Senha"
-            value=""
-          />
+          <InputItem>
+            <InputDiv>
+              <Envelope size={12} weight="fill" />
+              <Input
+                name="email"
+                type="email"
+                placeholder="Seu E-mail"
+                value=""
+              />
+            </InputDiv>
+          </InputItem>
+          <InputDiv>
+            <InputItem>
+              <LockSimple size={12} weight="fill" />
+              <Input
+                name="password"
+                type="password"
+                placeholder="Sua Senha"
+                value=""
+              />
+            </InputItem>
+          </InputDiv>
           <Button text="Entrar" margin="15px 0" />
           <SignUpText>
             Não tem conta?
--- frontend/src/pages/signin/style.ts
+++ frontend/src/pages/signin/style.ts
@@ -1,17 +1,26 @@
-import styled from "styled-components";
-import { Container } from "../../styles/layout/Container";
+import styled from 'styled-components'
+import { Container } from '../../styles/layout/Container'
 
 export const SignInWrapper = styled(Container)`
+  display: grid;
+  max-width: 777px;
+  height: 100vh;
+  grid-template-columns: 378px 1fr;
+  align-items: center;
+  justify-content: center;
 `
 
 export const SignInText = styled.p`
-
+  font-size: 3rem;
+  font-weight: bold;
+  color: #ffffff;
 `
 
 export const FormWrapper = styled.form`
-
+  display: block;
+  height: 400;
+  width: 378;
+  background-color: #1c1c1c;
 `
 
-export const SignUpText = styled.p`
-`
-
+export const SignUpText = styled.p``
