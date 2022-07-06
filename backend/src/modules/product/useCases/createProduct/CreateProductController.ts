27aa3d6c3627c5caa6d6d1b29e4740d0c93110b1
Italo Ferreira <grd788@gmail.com>
2022-07-05 23:20:07 -0500

    feat :sparkles: create product use case

--- backend/src/modules/product/useCases/createProduct/CreateProductController.ts
+++ backend/src/modules/product/useCases/createProduct/CreateProductController.ts
@@ -4,13 +4,17 @@ import { CreateProductUseCase } from './CreateProductUseCase'
 export class CreateProductController {
   async handle(request: Request, response: Response) {
     const { name, description, price } = request.body
+    let image = request?.file?.filename
+
+    if(!image) throw new Error('Imagem faltando, verifique.')
 
     const createProductUseCase = new CreateProductUseCase()
 
     const product = createProductUseCase.execute({
       name,
       description,
-      price
+      price,
+      image
     })
 
     return product
