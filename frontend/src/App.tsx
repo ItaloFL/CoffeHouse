f249b744dff448c73caebb82a80aec577fc20f46
Italo Ferreira <grd788@gmail.com>
2022-07-23 23:16:31 -0500

    feat :sparkles: app component using theme provider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,25 @@
 import { Home } from './components/Home'
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
 import { GlobalStyles } from './styles/globalStyles'
 import { SignUp } from './pages/signup'
 import { SignIn } from './pages/signin'
+import { light } from './styles/theme/light'
+import { dark } from './styles/theme/dark'
 
 export function App() {
   return (
     <>
-      <BrowserRouter>
-        <GlobalStyles />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/signin" element={<SignIn />} />
-        </Routes>
-      </BrowserRouter>
+      <ThemeProvider theme={dark}>
+        <BrowserRouter>
+          <GlobalStyles />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/signin" element={<SignIn />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
     </>
   )
 }
