0fedf670994f1b8591cad919dbcb4961fd49d066
Italo Ferreira <grd788@gmail.com>
2022-07-23 23:14:55 -0500

    style :ok_hand: theme dark and light config

---
+++ frontend/src/styles/theme/dark.ts
@@ -0,0 +1,14 @@
+export const dark = {
+  name: 'dark',
+  colors: {
+    background: '#121212',
+    secondaryColor: '#27272A',
+    cardColor: '#161616',
+    primaryTextColor: '#FFFFFF',
+    secondaryTextColor: '#A6AAAD',
+    formBackgroudColor: '#1C1C1C',
+    footerListTitleColor: '#666666',
+    footerListColor: '#A6AAAD',
+    borderColor: 'rgba(72, 72, 72, 0.5)'
+  }
+}
---
+++ frontend/src/styles/theme/light.ts
@@ -0,0 +1,14 @@
+export const light = {
+  name: 'light',
+  colors: {
+    background: '#EBEBEB',
+    secondaryColor: '#E7E7E7',
+    cardColor: '#F4F4F5',
+    primaryTextColor: '#27272A',
+    secondaryTextColor: '#3A3A3D',
+    formBackgroudColor: '#F4F4F5',
+    footerListTitleColor: '#27272A',
+    footerListColor: '#71717A',
+    borderColor: '#D4D4D8'
+  }
+}
