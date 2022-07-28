eeeb1aac7c38b9fa9140f123c23874c2e0cc38bf
Italo Ferreira <grd788@gmail.com>
2022-07-27 22:28:13 -0500

    feat :sparkles: background and styles color alters props

--- frontend/src/components/Button/style.ts
+++ frontend/src/components/Button/style.ts
@@ -3,11 +3,12 @@ import styled from 'styled-components'
 export const ButtonStyle = styled.button`
   width: 290px;
   height: 55px;
-  color: #ffffff;
+  color: ${props => props.theme.colors.primaryTextColor};
   background: linear-gradient(#5391ed, #154c9c);
   cursor: pointer;
   text-transform: uppercase;
   border-radius: 4px;
+  border: none;
 
   :hover, :focus {
     background: linear-gradient(#71AAFF, #1354B4);
