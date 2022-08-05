9053f6b7dff51f0f3786ed5da94d06de6ad55616
Italo Ferreira <grd788@gmail.com>
2022-08-05 13:43:14 -0500

    in progress :construction: Coffe component in contruction

---
+++ frontend/src/components/Coffes/index.tsx
@@ -0,0 +1,15 @@
+import { Title } from '../Title'
+import { CoffeWrapper } from './style'
+
+export function Coffe() {
+  return (
+    <CoffeWrapper>
+      <Title
+        Text="Vamos conhecer nossos tipos de 
+        café e fique por dentro de tudo!"
+        fontSize="48px"
+        maxWidth="726px"
+      />
+    </CoffeWrapper>
+  )
+}
---
+++ frontend/src/components/Coffes/style.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import { Container } from "../../styles/layout/Container";
+
+
+
+export const CoffeWrapper = styled(Container)`
+
+`
