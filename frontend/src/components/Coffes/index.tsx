0e57619f6806fcc7bfb695c08bf9427412aa7c2f
Italo Ferreira <grd788@gmail.com>
2022-08-18 23:53:42 -0500

    feat :sparkles: coffestype array create and in contruction

--- frontend/src/components/Coffes/index.tsx
+++ frontend/src/components/Coffes/index.tsx
@@ -1,5 +1,30 @@
 import { Title } from '../Title'
 import { CoffeWrapper } from './style'
+import tradicionalCoffeImage from '../../assets/tradicional.svg'
+import gourmetCoffeImage from '../../assets/gourmet.svg'
+import extraforteCoffeImage from '../../assets/extraforte.svg'
+
+const typesCoffe = 
+[
+  {
+    name: "Tradicional",
+    coffeIcon: tradicionalCoffeImage,
+    subtitle: "Conheça o nosso tipo de café tradicional",
+    description: "Aqui você verá toda nossa linha de produção e preços e imagens do nosso café."
+  },
+  {
+    name: "Gourmet",
+    coffeIcon: gourmetCoffeImage,
+    subtitle: "Conheça o nosso tipo de café gourmet",
+    description: "Aqui você verá toda nossa linha de produção e preços e imagens do nosso café."
+  },
+  {
+    name: "Extra Forte",
+    coffeIcon: extraforteCoffeImage,
+    subtitle: "Conheça o nosso tipo de café extraforte",
+    description: "Aqui você verá toda nossa linha de produção e preços e imagens do nosso café."
+  }
+]
 
 export function Coffe() {
   return (
