37eb7340e7742f2ba1c8d6f21071187a26382670
Italo Ferreira <grd788@gmail.com>
2022-08-27 23:10:04 -0500

    fix :bug: array don't save info and method return error

--- backend/src/repositories/inMemory/InMemoryUserRepository.ts
+++ backend/src/repositories/inMemory/InMemoryUserRepository.ts
@@ -1,39 +1,44 @@
 import { User } from '@prisma/client'
 import { UserRepository, UserType } from '../UserRepositories/userRepository'
+import { v4 } from 'uuid'
+import { AppError } from '../../errors/AppError/AppError'
 
 export class InMemoryUserRepository implements UserRepository {
-  private users: User[] = []
+  users: User[] = []
 
   async create({ name, email, telefone, password }: UserType): Promise<User> {
     const user = {} as User
 
     Object.assign(user as User, {
+      id: user.id ?? v4(),
       name,
       email,
       telefone,
       password
     })
 
+    this.users.push(user)
+
     return user
   }
 
-  async findUserByEmail(email: string): Promise<User | undefined> {
+  async findUserByEmail(email: string): Promise<User | null> {
     const user = this.users.find(user => user.email === email)
 
-    return user
+    return user ?? null
   }
 
-  async findUserById(id: string): Promise<User | undefined> {
+  async findUserById(id: string): Promise<User | null> {
     const user = this.users.find(user => user.id === id)
 
-    return user
+    return user ?? null
   }
 
   async update(id: string, name: string, telefone: string): Promise<User> {
     const user = await this.findUserById(id)
 
     if (!user) {
-      throw new Error('Usuário não encontrado')
+      throw new AppError('Usuário não encontrado')
     }
 
     user.name = name ?? user.name
