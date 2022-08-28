68e801b9d0aa23862f0a8230674677398a6714d2
Italo Ferreira <grd788@gmail.com>
2022-08-27 23:20:49 -0500

    feat :sparkles: class error AppError and express async errors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,27 @@
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
+import 'express-async-errors'
 import { routes } from './routes/routes'
+import cors from 'cors'
+import { AppError } from './errors/AppError/AppError'
 
 const app = express()
+app.use(cors())
+app.use(express.json())
 app.use(routes)
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.code).json({
+        message: err.message
+      })
+    }
+
+    return response.status(500).json({
+      message: `Server Error, ${err.message}`
+    })
+  }
+)
 
 app.listen(3333, () => {
   console.log('Server is running in port 3333')
