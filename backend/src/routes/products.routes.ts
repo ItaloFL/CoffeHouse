3d034f91991bc7491d056c1aa735648e8636ce64
Italo Ferreira <grd788@gmail.com>
2022-08-27 23:20:24 -0500

    feat :sparkles: products and users routes add

--- backend/src/routes/products.routes.ts
+++ backend/src/routes/products.routes.ts
@@ -1,12 +1,23 @@
-import { Router } from "express";
-import multer from "multer";
+import { Router } from 'express'
+import multer from 'multer'
 import configMulter from '../config/multer'
-import { CreateProductController } from "../modules/product/useCases/createProduct/CreateProductController";
+import { CreateProductController } from '../modules/product/useCases/createProduct/CreateProductController'
+import { EditProductController } from '../modules/product/useCases/editProduct/EditProductController'
 
 export const productsRouter = Router()
 
-const upload = multer(configMulter);
+const upload = multer(configMulter)
 
 const createProductController = new CreateProductController()
+const editProductController = new EditProductController()
 
-productsRouter.post('/products', upload.single('image'), createProductController.handle)
+productsRouter.post(
+  '/products',
+  upload.single('image'),
+  createProductController.handle
+)
+productsRouter.put(
+  '/products',
+  upload.single('image'),
+  editProductController.handle
+)
--- backend/src/routes/users.routes.ts
+++ backend/src/routes/users.routes.ts
@@ -1,8 +1,11 @@
-import { Router } from "express";
-import { CreateUserController } from "../modules/accounts/useCases/createUser/CreateUserController";
+import { Router } from 'express'
+import { CreateUserController } from '../modules/accounts/useCases/createUser/CreateUserController'
+import { AuthenticateUserController } from '../modules/accounts/useCases/authenticateUser/AuthenticateUserController'
 
 export const userRouter = Router()
 
 const createUserController = new CreateUserController()
+const authenticateUserController = new AuthenticateUserController()
 
 userRouter.post('/user', createUserController.handle)
+userRouter.post('/login', authenticateUserController.handle)
