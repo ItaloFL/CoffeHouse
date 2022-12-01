85601267d3afc934a18c14e17210a38cfb4d1f45
Italo Ferreira <grd788@gmail.com>
2022-11-30 23:45:52 -0500

    feat :sparkles: extraForte page and respositity done

--- frontend/src/pages/extraForteCoffe/index.tsx
+++ frontend/src/pages/extraForteCoffe/index.tsx
@@ -1,15 +1,27 @@
-import { Clock, Fire } from 'phosphor-react'
-import { TitleCoffe } from '../tradicionalCoffe/style'
+import { Clock, Fire, Plus } from 'phosphor-react'
 import {
+  ButtonDescription,
+  TitleCoffe,
+  TitleWrapper
+} from '../tradicionalCoffe/style'
+import {
+  AccordionRoot,
   DiferentialItemText,
   DiferentialItemTitle,
   DiferentialWrapper,
   ExtraForteCoffeWrapper,
+  FrequentesTitle,
+  ImageWrapper,
   IntroductionContent,
   IntroductionItemList,
   IntroductionList,
   IntroductionWrapper,
-  ListDifferentialWrapper
+  ListDifferentialWrapper,
+  ListTypeCoffeDescription,
+  ListTypeCoffeItem,
+  ListTypeCoffeTitle,
+  ListTypesCoffe,
+  StyledContent
 } from './style'
 import { Title } from '../../components/Title'
 import ExtraForteImgCoffe from '../../assets/extraForte.png'
@@ -18,6 +30,7 @@ import EntregaIcon from '../../assets/entrega.svg'
 import LocalIcon from '../../assets/local.svg'
 import { Header } from '../../components/Header'
 import { Footer } from '../../components/Footer'
+import * as Accordion from '@radix-ui/react-accordion'
 
 type Props = {
   toggleTheme(): void
@@ -41,15 +54,17 @@ export function ExtraForteCoffe({ toggleTheme }: Props) {
                 <p>Ate 6 horas de trabalho</p>
               </IntroductionItemList>
             </IntroductionList>
+            <ButtonDescription>Estou interressado</ButtonDescription>
           </IntroductionContent>
-          <img src={ExtraForteImgCoffe} alt="" />
+          <ImageWrapper>
+            <img src={ExtraForteImgCoffe} alt="" />
+          </ImageWrapper>
         </IntroductionWrapper>
 
         <DiferentialWrapper>
           <Title
             Text="O que nos diferencia
             dos demais cafés?"
-            fontSize="36px"
             margin="0 auto"
             marginBottom="120px"
           />
@@ -118,22 +133,121 @@ export function ExtraForteCoffe({ toggleTheme }: Props) {
         </DiferentialWrapper>
 
         <TitleCoffe>Os tipos de grãos do nosso café</TitleCoffe>
-        <ul>
-          <li>
-            <p>Grãos Arabicos</p>
-            <p>TextTest</p>
-          </li>
+        <ListTypesCoffe>
+          <ListTypeCoffeItem>
+            <ListTypeCoffeTitle>Grãos Arabicos</ListTypeCoffeTitle>
+            <ListTypeCoffeDescription>
+              Nossos grãos vindos diretamente da Arábia, para te trazer o melhor
+              sabor de lá
+            </ListTypeCoffeDescription>
+          </ListTypeCoffeItem>
 
-          <li>
-            <p>Grãos Arabicos</p>
-            <p>TextTest</p>
-          </li>
+          <ListTypeCoffeItem>
+            <ListTypeCoffeTitle>Graõs Mexicanos</ListTypeCoffeTitle>
+            <ListTypeCoffeDescription>
+              Nossos grãos vindos diretamente do Mexico, para te trazer o melhor
+              sabor de lá
+            </ListTypeCoffeDescription>
+          </ListTypeCoffeItem>
 
-          <li>
-            <p>Grãos Arabicos</p>
-            <p>TextTest</p>
-          </li>
-        </ul>
+          <ListTypeCoffeItem>
+            <ListTypeCoffeTitle>Grãos Brasileiros</ListTypeCoffeTitle>
+            <ListTypeCoffeDescription>
+              Nossos grãos vindos diretamente do Brasil, para te trazer o melhor
+              sabor de lá
+            </ListTypeCoffeDescription>
+          </ListTypeCoffeItem>
+        </ListTypesCoffe>
+
+        <TitleCoffe>Perguntas mais frequentes</TitleCoffe>
+        <AccordionRoot type="single" collapsible>
+          <Accordion.Item value="item-1">
+            <FrequentesTitle>
+              <TitleWrapper>
+                Qual é o café?
+                <Plus size={25} color="currentcolor" />
+              </TitleWrapper>
+            </FrequentesTitle>
+            <StyledContent>
+              O café atual é o Gourmet, feito com muito carinho desde a seleção
+              dos seus grãos
+            </StyledContent>
+          </Accordion.Item>
+          <Accordion.Item value="item-2">
+            <FrequentesTitle>
+              <TitleWrapper>
+                Existe alguma diferença entre os gourmet?
+                <Plus size={25} color="currentcolor" />
+              </TitleWrapper>
+            </FrequentesTitle>
+            <StyledContent>
+              Existem, principalmente no modo de preparo, algo que é feito em
+              todos os cafés é a escolha dos grãos sem erros, poré, alguns tem a
+              torrefação mais forte, e alguns uma maturação por maior tempo.
+            </StyledContent>
+          </Accordion.Item>
+          <Accordion.Item value="item-3">
+            <FrequentesTitle>
+              <TitleWrapper>
+                Entrega fora do Brasil?
+                <Plus size={25} color="currentcolor" />
+              </TitleWrapper>
+            </FrequentesTitle>
+            <StyledContent>
+              Sim, entregamos para fora do Brasil, basta na compra colocar o cep
+              do seu endereço e pagar a taxa de entrega que calculamos.
+            </StyledContent>
+          </Accordion.Item>
+          <Accordion.Item value="item-4">
+            <FrequentesTitle>
+              <TitleWrapper>
+                O que fazer se não houver estoque?
+                <Plus size={25} color="currentcolor" />
+              </TitleWrapper>
+            </FrequentesTitle>
+            <StyledContent>
+              Pode nos mandar uma mensagem em uma de nossas redes sociais, e
+              mandaremos para você um calendairo com os possiveis meses em que
+              teremos estoque de volta
+            </StyledContent>
+          </Accordion.Item>
+          <Accordion.Item value="item-5">
+            <FrequentesTitle>
+              <TitleWrapper>
+                Quais as formas de pagamento?
+                <Plus size={25} color="currentcolor" />
+              </TitleWrapper>
+            </FrequentesTitle>
+            <StyledContent>
+              A nossas formas de pagamento são completas, tanto cartões de
+              debito e credito, boletos, e pix.
+            </StyledContent>
+          </Accordion.Item>
+          <Accordion.Item value="item-6">
+            <FrequentesTitle>
+              <TitleWrapper>
+                Em quanto tempo chega o café?
+                <Plus size={25} color="currentcolor" />
+              </TitleWrapper>
+            </FrequentesTitle>
+            <StyledContent>
+              Não podemos dar uma data exata por conta de possiveis problemas,
+              mas tentamos sempre fazer a entrega em ate 15 dias uteis?
+            </StyledContent>
+          </Accordion.Item>
+          <Accordion.Item value="item-7">
+            <FrequentesTitle>
+              <TitleWrapper>
+                O café não chegou, o que faço?
+                <Plus size={25} color="currentcolor" />
+              </TitleWrapper>
+            </FrequentesTitle>
+            <StyledContent>
+              Nos contacte o mais rapido para que vejamos qual o erro foi
+              cometido e como podemos concerta-lo, o cliente tem a prioridade
+            </StyledContent>
+          </Accordion.Item>
+        </AccordionRoot>
       </ExtraForteCoffeWrapper>
       <Footer />
     </>
--- frontend/src/pages/extraForteCoffe/style.ts
+++ frontend/src/pages/extraForteCoffe/style.ts
@@ -1,5 +1,6 @@
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 import { Container } from '../../styles/layout/Container'
+import * as Accordion from '@radix-ui/react-accordion'
 
 export const ExtraForteCoffeWrapper = styled(Container)``
 
@@ -8,7 +9,11 @@ export const IntroductionContent = styled.div``
 export const IntroductionList = styled.ul`
   display: flex;
   gap: 2.5rem;
-  margin-top: 5.625rem;
+  margin-top: 2.625rem;
+
+  @media (max-width: 968px) {
+    flex-direction: column;
+  }
 `
 
 export const IntroductionItemList = styled.li`
@@ -17,9 +22,22 @@ export const IntroductionItemList = styled.li`
   color: ${props => props.theme.colors.primaryTextColor};
 `
 
+export const ImageWrapper = styled.div`
+  @media (max-width: 860px) {
+    img {
+      display: none;
+    }
+  }
+`
+
 export const DiferentialWrapper = styled.div`
   margin-top: 10rem;
   margin-bottom: 10rem;
+
+  @media (max-width: 968px) {
+    margin-top: 8rem;
+    margin-bottom: 8rem;
+  }
 `
 
 export const ListDifferentialWrapper = styled.div`
@@ -27,6 +45,14 @@ export const ListDifferentialWrapper = styled.div`
   grid-template-columns: repeat(3, 1fr);
   column-gap: 8.75rem;
   row-gap: 3.125rem;
+
+  @media (max-width: 768px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media (max-width: 610px) {
+    grid-template-columns: 1fr;
+  }
 `
 
 export const IntroductionWrapper = styled.div`
@@ -34,6 +60,11 @@ export const IntroductionWrapper = styled.div`
   gap: 7.5rem;
   margin-top: 10rem;
   margin-bottom: 10rem;
+
+  @media (max-width: 968px) {
+    margin-top: 8rem;
+    margin-bottom: 8rem;
+  }
 `
 
 export const DiferentialItemTitle = styled.h2`
@@ -42,10 +73,96 @@ export const DiferentialItemTitle = styled.h2`
   margin-bottom: 1rem;
   font-weight: bold;
   color: ${props => props.theme.colors.primaryTextColor};
+
+  @media (max-width: 968px) {
+    font-size: 1.25rem;
+  }
 `
 
 export const DiferentialItemText = styled.p`
   font-family: var(--type-second);
   font-size: 1rem;
   color: ${props => props.theme.colors.secondaryTextColor};
+`
+
+export const ListTypesCoffe = styled.ul`
+  display: flex;
+  gap: 40px;
+  margin-top: 120px;
+  margin-bottom: 120px;
+
+  @media (max-width: 768px) {
+    flex-wrap: wrap;
+    margin-top: 60px;
+  }
+`
+
+export const ListTypeCoffeItem = styled.div``
+
+export const ListTypeCoffeTitle = styled.p`
+  font-size: 1.75rem;
+  color: ${props => props.theme.colors.primaryTextColor};
+  font-weight: bold;
+  margin-bottom: 10px;
+
+  @media (max-width: 968px) {
+    font-size: 1.25rem;
+  }
+`
+
+export const ListTypeCoffeDescription = styled.p`
+  font-family: var(--type-second);
+  font-size: 1.25rem;
+  color: ${props => props.theme.colors.secondaryTextColor};
+
+  @media (max-width: 968px) {
+    font-size: 1rem;
+  }
+`
+
+const slideDown = keyframes({
+  from: { height: 0 },
+  to: {
+    height: 'var(--radix-accordion-content-height)'
+  }
+})
+
+const slideUp = keyframes({
+  from: { height: 'var(--radix-accordion-content-height)' },
+  to: { height: 0 }
+})
+
+export const AccordionRoot = styled(Accordion.Root)`
+  margin-bottom: 80px;
+`
+
+export const FrequentesTitle = styled(Accordion.Trigger)`
+  border: none;
+  width: 100%;
+  height: 65px;
+  cursor: pointer;
+  text-align: start;
+  padding: 0 20px;
+  border-radius: 4px;
+  font-size: 1.2rem;
+  margin-top: 20px;
+  background-color: ${props => props.theme.colors.cardColor};
+  color: ${props => props.theme.colors.primaryTextColor};
+`
+
+export const StyledContent = styled(Accordion.Content)`
+  overflow: hidden;
+  font-size: 15;
+  padding: 30px;
+  line-height: 2rem;
+  color: ${props => props.theme.colors.primaryTextColor};
+  background-color: ${props => props.theme.colors.secondaryColor};
+
+  &[data-state='open'] {
+    animation: ${slideDown} 300ms;
+  }
+
+  &[data-state='close'] {
+    animation: ${slideUp} 300ms;
+  }
 `
